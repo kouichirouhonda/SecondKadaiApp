af5a7277fd5c563541334cc1b4af22acdb565ed4
KoichiroHonda <koichiro.h1031@gmail.com>
2020-09-18 13:21:16 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by KOICHIRO HONDA on 2020/09/18.
+//  Copyright © 2020 KOICHIRO HONDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
